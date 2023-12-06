74de29bb68f70a6c17fb344814257eccd88c20d4
karla aqueveque <aquevequekarla@gmail.com>
2023-12-06 20:41:58 -0300

    reset test

---
+++ src/app/components/home/home.component.spect.ts
@@ -0,0 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HomeComponent } from './home.component';
+import { HeaderComponent } from '../header/header.component';
+
+import { CardMoviesComponent } from '../card-movies/card-movies.component';
+import { ApiService } from 'src/app/services/api.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HomeComponent, HeaderComponent, CardMoviesComponent],
+      providers: [ApiService],
+      imports: [
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+        MatPaginatorModule,
+      ],
+    });
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
